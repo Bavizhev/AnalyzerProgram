753a00a49091401576695f48c27818f0d351dabe
Bavizhev <ilyas4bavizhev@gmail.com>
2023-11-04 22:49:20 +0300

    Bavizhev v0.1

--- src/main/java/org/example/TextAnalyser.java
+++ src/main/java/org/example/TextAnalyser.java
@@ -1,21 +1,30 @@
 package org.example;
+
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class TextAnalyser {
     // Создаем блокирующие очереди для символов 'a', 'b' и 'c'
-    private static ArrayBlockingQueue<String> queueA = new ArrayBlockingQueue<>(100);
-    private static ArrayBlockingQueue<String> queueB = new ArrayBlockingQueue<>(100);
-    private static ArrayBlockingQueue<String> queueC = new ArrayBlockingQueue<>(100);
+    private static ArrayBlockingQueue<String> queueA = new ArrayBlockingQueue<>(10);
+    private static ArrayBlockingQueue<String> queueB = new ArrayBlockingQueue<>(10);
+    private static ArrayBlockingQueue<String> queueC = new ArrayBlockingQueue<>(10);
+
+    private static AtomicReference<String> maxTextA = new AtomicReference<>("");
+    private static AtomicReference<String> maxTextB = new AtomicReference<>("");
+    private static AtomicReference<String> maxTextC = new AtomicReference<>("");
 
     public static void main(String[] args) {
+
+        String letters = "abc";
+        int length = 100_000;
+        int numbtexts = 10_000;
+
         // Создаем поток для генерации текстов
         Thread textGeneratorThread = new Thread(() -> {
-            String letters = "abc";
-            int length = 100000;
 
             Random random = new Random();
-            for (int i = 0; i < 10000; i++) {
+            for (int i = 0; i < numbtexts; i++) {
                 StringBuilder text = new StringBuilder();
                 for (int j = 0; j < length; j++) {
                     text.append(letters.charAt(random.nextInt(letters.length())));
@@ -23,15 +32,10 @@ public class TextAnalyser {
 
                 // Помещаем сгенерированный текст в соответствующую очередь
                 try {
-                    for (char c : text.toString().toCharArray()) {
-                        if (c == 'a') {
-                            queueA.put(text.toString());
-                        } else if (c == 'b') {
-                            queueB.put(text.toString());
-                        } else if (c == 'c') {
-                            queueC.put(text.toString());
-                        }
-                    }
+                    // Поменял добавление в очередь без проверки
+                    queueA.put(String.valueOf(text));
+                    queueB.put(String.valueOf(text));
+                    queueC.put(String.valueOf(text));
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -41,15 +45,14 @@ public class TextAnalyser {
         // Создаем поток для анализа символа 'a'
         Thread threadA = new Thread(() -> {
             int maxCountA = 0;
-            String maxTextA = "";
 
-            while (true) {
+            for (int i = 0; i < numbtexts; i++) {
                 try {
                     String text = queueA.take();
                     int countA = text.length() - text.replace("a", "").length();
                     if (countA > maxCountA) {
                         maxCountA = countA;
-                        maxTextA = text;
+                        maxTextA.set(text);
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -60,15 +63,14 @@ public class TextAnalyser {
         // Создаем поток для анализа символа 'b'
         Thread threadB = new Thread(() -> {
             int maxCountB = 0;
-            String maxTextB = "";
 
-            while (true) {
+            for (int i = 0; i < numbtexts; i++) {
                 try {
                     String text = queueB.take();
                     int countB = text.length() - text.replace("b", "").length();
                     if (countB > maxCountB) {
                         maxCountB = countB;
-                        maxTextB = text;
+                        maxTextB.set(text);
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -79,15 +81,14 @@ public class TextAnalyser {
         // Создаем поток для анализа символа 'c'
         Thread threadC = new Thread(() -> {
             int maxCountC = 0;
-            String maxTextC = "";
 
-            while (true) {
+            for (int i = 0; i < numbtexts; i++) {
                 try {
                     String text = queueC.take();
                     int countC = text.length() - text.replace("c", "").length();
                     if (countC > maxCountC) {
                         maxCountC = countC;
-                        maxTextC = text;
+                        maxTextC.set(text);
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -100,5 +101,18 @@ public class TextAnalyser {
         threadA.start();
         threadB.start();
         threadC.start();
+
+        try {
+            textGeneratorThread.join();
+            threadA.join();
+            threadB.join();
+            threadC.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Текст, в котором содержится максимальное количество символов 'a' : " + maxTextA);
+        System.out.println("Текст, в котором содержится максимальное количество символов 'b' : " + maxTextB);
+        System.out.println("Текст, в котором содержится максимальное количество символов 'c' : " + maxTextC);
     }
 }
